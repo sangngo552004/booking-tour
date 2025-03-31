cfc86135b29718585bb2d026a64797c2094cce64
sangn <sangngo552004@gmail.com>
2025-03-31 23:04:29 +0700

    homepage

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 window.addEventListener('scroll', function() {
     const navbar = document.querySelector('.navbar');
+    let filterBox = document.querySelector(".filter-box");
     if (window.scrollY > 50) { /* Khi cuộn quá 50px */
         navbar.classList.add('scrolled');
+        filterBox.classList.add("show");
     } else {
         navbar.classList.remove('scrolled');
+        filterBox.classList.remove("show");
     }
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
